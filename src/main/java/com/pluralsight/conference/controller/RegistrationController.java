d7225265e82c5bb49b38d5cfacbdda801e919cd4
Marcus Maciel <marcus.maciel@ab-inbev.com>
2023-11-02 00:22:14 -0300

    validations in spring mvc

--- src/main/java/com/pluralsight/conference/controller/RegistrationController.java
+++ src/main/java/com/pluralsight/conference/controller/RegistrationController.java
@@ -1,7 +1,9 @@
 package com.pluralsight.conference.controller;
 
 import com.pluralsight.conference.model.Registration;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +17,13 @@ public class RegistrationController {
     }
 
     @PostMapping("registration")
-    public String addRegistration(@ModelAttribute("registration") Registration model) {
+    public String addRegistration(@Valid @ModelAttribute("registration") Registration model,
+                                  BindingResult result) {
+        if (result.hasErrors()) {
+            System.out.println("There were errors!");
+            return "registration";
+        }
+
         System.out.println("Registration: " + model.getName());
         return "redirect:registration";
     }
--- src/main/webapp/WEB-INF/jsp/registration.jsp
+++ src/main/webapp/WEB-INF/jsp/registration.jsp
@@ -6,14 +6,28 @@
     <head>
         <meta charset="UTF-8">
         <title>Registration</title>
+        <style>
+            .error {
+                color: #ff0000;
+            }
+            .errorBlock {
+                color: #000;
+                background-color: #ffEEEE;
+                border: 3px solid #ff0000;
+                padding: 8px;
+                margin: 16px;
+            }
+        </style>
    </head>
    <body>
         <h1>Registration</h1>
         <form:form modelAttribute="registration">
+            <form:errors path="*" cssClass="errorBlock" element="div" />
             <table>
                 <tr>
                     <td><form:label path="name"><spring:message code="name" /></form:label></td>
                     <td><form:input path="name" /></td>
+                    <td><form:errors path="name" cssClass="error" /></td>
                 </tr>
                 <tr>
                     <td colspan="2">
